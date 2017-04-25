cc1f1e6f8cd7763963fbb68a5aafe636c5155578
Arko <dotuplink@gmail.com>
2017-04-24 22:53:48 -0700

    Housekeeping

--- Philo_Sim.py
+++ Philo_Sim.py
@@ -8,7 +8,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Natural Constants
-R = 8.314	# J/(K*mol)
+R = 8.314	# J/(K*mol) or ((Kg*m^2)/s^2)/(K*mol)
 g = 9.81	# m/s^2
 P_amb = 101325	# Pa
 T_amb = 298.15	# K
@@ -59,8 +59,8 @@ tanks = {'NIN-90-4500SO-GG':Tank(0.00147484, 3.1026e+7, 1.49, 3.103e+6, 5.516e+6
 	 'NIN-NINJAGREY68D':Tank(0.00111432, 3.1026e+7, 1.36, 3.103e+6, 5.516e+6, 298.15), \
 	 'NIN-NINJAGREY50D':Tank(0.00081935, 3.1026e+7, 1.04, 3.103e+6, 5.516e+6, 298.15)}
 
-fuel = {'Air':Fuel("Compressed Dry Air", 1.005, 0.718, 28.97), \
-	'CO2':Fuel("Carbon Dioxide", 0.846, 0.657, 44.01)}
+fuel = {'Air':Fuel("Compressed Dry Air", 1.005, 0.718, 0.02897), \
+	'CO2':Fuel("Carbon Dioxide", 0.846, 0.657, 0.04401)}
 
 #regulator = {'':
 
@@ -82,7 +82,7 @@ tank_mass = 1.49		# kg
 fuel_type = "Compressed Dry Air"
 cp = 1.005
 cv = 0.718
-fuel_molar_mass = 28.97   	# kg/mol
+fuel_molar_mass = 0.02897   	# kg/mol
 specific_heat_ratio = cp/cv
 fuel_mass = ((tank_press*tank_vol)/(R*tank_temp))*fuel_molar_mass
 
@@ -105,25 +105,18 @@ flight_time = 0			# sec
 
 # Flight Sim
 # ----------------------------------------
-print "Initial Fuel Mass (kg): "
-print fuel_mass
-
-print "Initial Vehicle Wet Mass (kg): "
-print veh_mass
-
-print "Exhaust Mass Flow (kg/s): "
-print mass_flow
+print "Initial Fuel Mass (kg): \t %.8f" % fuel_mass
+print "Initial Vehicle Wet Mass (kg): \t %.8f" % wet_mass
+print "Exhaust Mass Flow (kg/s): \t %.8f" % mass_flow
 
 # Using Rocket Equation
-delta_v = V_e*np.log(wet_mass/dry_mass)
-print "Delta V (m/s): "
-print delta_v
+#delta_v = V_e*np.log(wet_mass/dry_mass)
+#print "Delta V (m/s): \t\t\t %.8f" % delta_v
 
 # Using Mass Flow
 flight_time = fuel_mass/mass_flow
-print "Flight Time (sec): "
-print flight_time
-flight_time = 0
+print "Flight Time (sec): \t\t %.8f" % flight_time
+flight_time = 0.0
 
 # Using Numerical Approx, mass loss thrust compensation
 # 	This function compensates for the change in thrust due to
@@ -135,11 +128,9 @@ while (fuel_mass > 0):
 	mass_flow = F_null/V_e
 	fuel_mass -= mass_flow*dt
 	flight_time += dt 
-	#print F_null
+	#print veh_mass
 	
-print "END OF FLIGHT"
-print "Flight Time (sec): "
-print flight_time	
+print "Flight Time (sec): \t\t %.8f" % flight_time
 # ----------------------------------------
 
 #x = np.zeros ( (n,3) )
